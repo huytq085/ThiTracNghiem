83174e9e578fff2f29f619609d414e2ddac6b53c
Nguyen Tan Dung <TanDung847@gmail.com>
2017-11-28 01:16:45 +0700

    Update FrmThi , fix DataSource dubplicate of member in combobox and set possition to label,cmb,btn...

--- ThiTracNghiem/Forms/FrmThi.Designer.cs
+++ ThiTracNghiem/Forms/FrmThi.Designer.cs
@@ -37,19 +37,21 @@
             this.cmbNGAYTHI = new System.Windows.Forms.ComboBox();
             this.cmbTENMONHOC = new System.Windows.Forms.ComboBox();
             this.bdsV_TENMHTHI = new System.Windows.Forms.BindingSource(this.components);
-            this.label3 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
+            this.lbDESLANTHI = new System.Windows.Forms.Label();
+            this.lbDESNGAYTHI = new System.Windows.Forms.Label();
+            this.lbDESMONHOC = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
             this.lbSOCAUTHI = new System.Windows.Forms.Label();
             this.lbTRINHDO = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
+            this.lbDESTHOIGIAN = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.panel3 = new System.Windows.Forms.Panel();
             this.gIAOVIEN_DANGKYTableAdapter = new ThiTracNghiem.DS_SERVER1TableAdapters.GIAOVIEN_DANGKYTableAdapter();
             this.tableAdapterManager = new ThiTracNghiem.DS_SERVER1TableAdapters.TableAdapterManager();
             this.v_TENMHTHITableAdapter = new ThiTracNghiem.DS_SERVER1TableAdapters.V_TENMHTHITableAdapter();
+            this.btnCHONLAI = new System.Windows.Forms.Button();
+            this.btnNOPBAI = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.bdsGVDK)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dS_SERVER1)).BeginInit();
@@ -60,25 +62,26 @@
             // panel1
             // 
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.btnCHONLAI);
             this.panel1.Controls.Add(this.btnBATDAUTHI);
             this.panel1.Controls.Add(this.cmbLANTHI);
             this.panel1.Controls.Add(this.cmbNGAYTHI);
             this.panel1.Controls.Add(this.cmbTENMONHOC);
-            this.panel1.Controls.Add(this.label3);
-            this.panel1.Controls.Add(this.label2);
-            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.lbDESLANTHI);
+            this.panel1.Controls.Add(this.lbDESNGAYTHI);
+            this.panel1.Controls.Add(this.lbDESMONHOC);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Margin = new System.Windows.Forms.Padding(4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1350, 80);
+            this.panel1.Size = new System.Drawing.Size(1370, 80);
             this.panel1.TabIndex = 0;
             // 
             // btnBATDAUTHI
             // 
-            this.btnBATDAUTHI.Location = new System.Drawing.Point(1152, 16);
+            this.btnBATDAUTHI.Location = new System.Drawing.Point(1175, 16);
             this.btnBATDAUTHI.Name = "btnBATDAUTHI";
-            this.btnBATDAUTHI.Size = new System.Drawing.Size(182, 46);
+            this.btnBATDAUTHI.Size = new System.Drawing.Size(179, 46);
             this.btnBATDAUTHI.TabIndex = 6;
             this.btnBATDAUTHI.Text = "BẮT ĐẦU THI";
             this.btnBATDAUTHI.UseVisualStyleBackColor = true;
@@ -86,12 +89,11 @@
             // 
             // cmbLANTHI
             // 
-            this.cmbLANTHI.DataBindings.Add(new System.Windows.Forms.Binding("SelectedValue", this.bdsGVDK, "LAN", true));
             this.cmbLANTHI.DataSource = this.bdsGVDK;
             this.cmbLANTHI.DisplayMember = "LAN";
             this.cmbLANTHI.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbLANTHI.FormattingEnabled = true;
-            this.cmbLANTHI.Location = new System.Drawing.Point(956, 27);
+            this.cmbLANTHI.Location = new System.Drawing.Point(811, 27);
             this.cmbLANTHI.Name = "cmbLANTHI";
             this.cmbLANTHI.Size = new System.Drawing.Size(145, 25);
             this.cmbLANTHI.TabIndex = 5;
@@ -110,12 +112,11 @@
             // 
             // cmbNGAYTHI
             // 
-            this.cmbNGAYTHI.DataBindings.Add(new System.Windows.Forms.Binding("SelectedValue", this.bdsGVDK, "NGAYTHI", true));
             this.cmbNGAYTHI.DataSource = this.bdsGVDK;
             this.cmbNGAYTHI.DisplayMember = "NGAYTHI";
             this.cmbNGAYTHI.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbNGAYTHI.FormattingEnabled = true;
-            this.cmbNGAYTHI.Location = new System.Drawing.Point(629, 27);
+            this.cmbNGAYTHI.Location = new System.Drawing.Point(547, 27);
             this.cmbNGAYTHI.Name = "cmbNGAYTHI";
             this.cmbNGAYTHI.Size = new System.Drawing.Size(157, 25);
             this.cmbNGAYTHI.TabIndex = 4;
@@ -124,12 +125,11 @@
             // 
             // cmbTENMONHOC
             // 
-            this.cmbTENMONHOC.DataBindings.Add(new System.Windows.Forms.Binding("SelectedValue", this.bdsV_TENMHTHI, "TENMH", true));
             this.cmbTENMONHOC.DataSource = this.bdsV_TENMHTHI;
             this.cmbTENMONHOC.DisplayMember = "TENMH";
             this.cmbTENMONHOC.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbTENMONHOC.FormattingEnabled = true;
-            this.cmbTENMONHOC.Location = new System.Drawing.Point(141, 27);
+            this.cmbTENMONHOC.Location = new System.Drawing.Point(115, 27);
             this.cmbTENMONHOC.Name = "cmbTENMONHOC";
             this.cmbTENMONHOC.Size = new System.Drawing.Size(307, 25);
             this.cmbTENMONHOC.TabIndex = 3;
@@ -141,43 +141,44 @@
             this.bdsV_TENMHTHI.DataMember = "V_TENMHTHI";
             this.bdsV_TENMHTHI.DataSource = this.dS_SERVER1;
             // 
-            // label3
+            // lbDESLANTHI
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(837, 31);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(68, 17);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "LẦN THI";
+            this.lbDESLANTHI.AutoSize = true;
+            this.lbDESLANTHI.Location = new System.Drawing.Point(719, 31);
+            this.lbDESLANTHI.Name = "lbDESLANTHI";
+            this.lbDESLANTHI.Size = new System.Drawing.Size(68, 17);
+            this.lbDESLANTHI.TabIndex = 2;
+            this.lbDESLANTHI.Text = "LẦN THI";
             // 
-            // label2
+            // lbDESNGAYTHI
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(499, 31);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(79, 17);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "NGÀY THI";
+            this.lbDESNGAYTHI.AutoSize = true;
+            this.lbDESNGAYTHI.Location = new System.Drawing.Point(441, 31);
+            this.lbDESNGAYTHI.Name = "lbDESNGAYTHI";
+            this.lbDESNGAYTHI.Size = new System.Drawing.Size(79, 17);
+            this.lbDESNGAYTHI.TabIndex = 1;
+            this.lbDESNGAYTHI.Text = "NGÀY THI";
             // 
-            // label1
+            // lbDESMONHOC
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(11, 31);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(79, 17);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "MÔN HỌC";
+            this.lbDESMONHOC.AutoSize = true;
+            this.lbDESMONHOC.Location = new System.Drawing.Point(10, 31);
+            this.lbDESMONHOC.Name = "lbDESMONHOC";
+            this.lbDESMONHOC.Size = new System.Drawing.Size(79, 17);
+            this.lbDESMONHOC.TabIndex = 0;
+            this.lbDESMONHOC.Text = "MÔN HỌC";
             // 
             // panel2
             // 
             this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.btnNOPBAI);
             this.panel2.Controls.Add(this.lbSOCAUTHI);
             this.panel2.Controls.Add(this.lbTRINHDO);
-            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.lbDESTHOIGIAN);
             this.panel2.Controls.Add(this.label5);
             this.panel2.Controls.Add(this.label4);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
-            this.panel2.Location = new System.Drawing.Point(1049, 80);
+            this.panel2.Location = new System.Drawing.Point(1069, 80);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(301, 376);
             this.panel2.TabIndex = 1;
@@ -200,15 +201,15 @@
             this.lbTRINHDO.TabIndex = 3;
             this.lbTRINHDO.Text = "lbTRINHDO";
             // 
-            // label6
+            // lbDESTHOIGIAN
             // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Times New Roman", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(77, 153);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(148, 31);
-            this.label6.TabIndex = 2;
-            this.label6.Text = "THỜI GIAN";
+            this.lbDESTHOIGIAN.AutoSize = true;
+            this.lbDESTHOIGIAN.Font = new System.Drawing.Font("Times New Roman", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbDESTHOIGIAN.Location = new System.Drawing.Point(77, 128);
+            this.lbDESTHOIGIAN.Name = "lbDESTHOIGIAN";
+            this.lbDESTHOIGIAN.Size = new System.Drawing.Size(148, 31);
+            this.lbDESTHOIGIAN.TabIndex = 2;
+            this.lbDESTHOIGIAN.Text = "THỜI GIAN";
             // 
             // label5
             // 
@@ -233,7 +234,7 @@
             this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel3.Location = new System.Drawing.Point(0, 80);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(1049, 376);
+            this.panel3.Size = new System.Drawing.Size(1069, 376);
             this.panel3.TabIndex = 2;
             // 
             // gIAOVIEN_DANGKYTableAdapter
@@ -258,12 +259,31 @@
             // 
             this.v_TENMHTHITableAdapter.ClearBeforeFill = true;
             // 
+            // btnCHONLAI
+            // 
+            this.btnCHONLAI.Location = new System.Drawing.Point(989, 16);
+            this.btnCHONLAI.Name = "btnCHONLAI";
+            this.btnCHONLAI.Size = new System.Drawing.Size(179, 46);
+            this.btnCHONLAI.TabIndex = 7;
+            this.btnCHONLAI.Text = "CHỌN LẠI";
+            this.btnCHONLAI.UseVisualStyleBackColor = true;
+            this.btnCHONLAI.Click += new System.EventHandler(this.btnCHONLAI_Click);
+            // 
+            // btnNOPBAI
+            // 
+            this.btnNOPBAI.Location = new System.Drawing.Point(60, 316);
+            this.btnNOPBAI.Name = "btnNOPBAI";
+            this.btnNOPBAI.Size = new System.Drawing.Size(179, 46);
+            this.btnNOPBAI.TabIndex = 5;
+            this.btnNOPBAI.Text = "NỘP BÀI";
+            this.btnNOPBAI.UseVisualStyleBackColor = true;
+            // 
             // FrmThi
             // 
             this.Appearance.Options.UseFont = true;
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1350, 456);
+            this.ClientSize = new System.Drawing.Size(1370, 456);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
@@ -291,21 +311,23 @@
         private System.Windows.Forms.ComboBox cmbLANTHI;
         private System.Windows.Forms.ComboBox cmbNGAYTHI;
         private System.Windows.Forms.ComboBox cmbTENMONHOC;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbDESLANTHI;
+        private System.Windows.Forms.Label lbDESNGAYTHI;
+        private System.Windows.Forms.Label lbDESMONHOC;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label lbSOCAUTHI;
-        private System.Windows.Forms.Label lbTRINHDO;
+        private System.Windows.Forms.Label lbDESTHOIGIAN;
         private DS_SERVER1 dS_SERVER1;
         private System.Windows.Forms.BindingSource bdsGVDK;
         private DS_SERVER1TableAdapters.GIAOVIEN_DANGKYTableAdapter gIAOVIEN_DANGKYTableAdapter;
         private DS_SERVER1TableAdapters.TableAdapterManager tableAdapterManager;
         private System.Windows.Forms.BindingSource bdsV_TENMHTHI;
         private DS_SERVER1TableAdapters.V_TENMHTHITableAdapter v_TENMHTHITableAdapter;
+        public System.Windows.Forms.Label lbSOCAUTHI;
+        public System.Windows.Forms.Label lbTRINHDO;
+        private System.Windows.Forms.Button btnCHONLAI;
+        private System.Windows.Forms.Button btnNOPBAI;
     }
 }
--- ThiTracNghiem/Forms/FrmThi.cs
+++ ThiTracNghiem/Forms/FrmThi.cs
@@ -17,24 +17,73 @@ namespace ThiTracNghiem.Forms
         int thuTu = 0;
         public FrmThi()
         {
+            
             InitializeComponent();
+
+            int xPanel1 = 10;
+            int yPanel1 = (panel1.Height - lbDESMONHOC.Height) / 2;
+            lbDESMONHOC.Location = new Point(xPanel1,yPanel1 );
+
+            xPanel1 += lbDESMONHOC.Width + 20;
+            yPanel1 = (panel1.Height - cmbTENMONHOC.Height) / 2;
+            cmbTENMONHOC.Location = new Point(xPanel1,yPanel1);
+
+            xPanel1 += cmbTENMONHOC.Width + 20;
+            yPanel1 = (panel1.Height - lbDESNGAYTHI.Height) / 2;
+            lbDESNGAYTHI.Location = new Point(xPanel1, yPanel1);
+
+            xPanel1 += lbDESNGAYTHI.Width + 20;
+            yPanel1 = (panel1.Height -cmbNGAYTHI.Height) / 2;
+            cmbNGAYTHI.Location = new Point(xPanel1, yPanel1);
+
+            xPanel1 += cmbNGAYTHI.Width + 20;
+            yPanel1 = (panel1.Height - lbDESLANTHI.Height) / 2;
+            lbDESLANTHI.Location = new Point(xPanel1, yPanel1);
+
+            xPanel1 += lbDESLANTHI.Width + 20;
+            yPanel1 = (panel1.Height - cmbLANTHI.Height) / 2;
+            cmbLANTHI.Location = new Point(xPanel1, yPanel1);
+
+            xPanel1 =panel1.Width-btnBATDAUTHI.Width- 20;
+            yPanel1 = (panel1.Height - btnBATDAUTHI.Height) / 2;
+            btnBATDAUTHI.Location = new Point(xPanel1, yPanel1);
+
+            xPanel1 -= btnBATDAUTHI.Width+20;
+            yPanel1 = (panel1.Height - btnCHONLAI.Height) / 2;
+            btnCHONLAI.Location = new Point(xPanel1, yPanel1);
+
+            lbDESTHOIGIAN.Location=new Point((panel2.Width-lbDESTHOIGIAN.Width)/2,(panel2.Height/2-50));
+
+            int xPanel2 = panel2.Width-btnNOPBAI.Width;
+            int yPanel2 = panel2.Height;
+            btnNOPBAI.Location = new Point(xPanel2/2,yPanel2);
         }
 
         private void btnBATDAUTHI_Click(object sender, EventArgs e)
         {
-
+            btnCHONLAI.Enabled = false;
+            btnBATDAUTHI.Enabled = false;
+            btnNOPBAI.Enabled = true;
         }
 
         private void FrmThi_Load(object sender, EventArgs e)
         {
             // TODO: This line of code loads data into the 'dS_SERVER1.V_TENMHTHI' table. You can move, or remove it, as needed.
-           
+
             // TODO: This line of code loads data into the 'dS_SERVER1.GIAOVIEN_DANGKY' table. You can move, or remove it, as needed.
             this.dS_SERVER1.EnforceConstraints = false;
             this.v_TENMHTHITableAdapter.Connection.ConnectionString = Program.connstr;
             this.v_TENMHTHITableAdapter.Fill(this.dS_SERVER1.V_TENMHTHI);
             this.gIAOVIEN_DANGKYTableAdapter.Connection.ConnectionString = Program.connstr;
             this.gIAOVIEN_DANGKYTableAdapter.Fill(this.dS_SERVER1.GIAOVIEN_DANGKY);
+            cmbLANTHI.SelectedIndex = -1;
+            cmbNGAYTHI.SelectedIndex = -1;
+            cmbTENMONHOC.SelectedIndex = -1;
+            cmbTENMONHOC.Enabled = true;
+            cmbLANTHI.Enabled = false;
+            cmbNGAYTHI.Enabled = false;
+            btnBATDAUTHI.Enabled = false;
+            btnNOPBAI.Enabled = false;
             lbSOCAUTHI.Text = "";
             lbTRINHDO.Text = "";
         }
@@ -46,43 +95,107 @@ namespace ThiTracNghiem.Forms
             this.tableAdapterManager.UpdateAll(this.dS_SERVER1);
 
         }
-
-        private void cmbTENMONHOC_SelectedIndexChanged(object sender, EventArgs e)
+        public static String timMAMH(String tenMH)
         {
             String MAMH = "";
-            String strLenh = "EXEC SP_TIMMALOP N"+cmbTENMONHOC.SelectedValue.ToString()+"'";
+            String strLenh = "EXEC SP_TIMMALOP N'" + tenMH + "'";
             SqlDataReader reader = Program.ExecSqlDataReader(strLenh);
             Boolean rd = reader.Read();
             if (rd)
             {
-                MAMH= reader["MAMH"].ToString();
+                MAMH = reader[0].ToString();
             }
 
             Program.conn.Close();
             reader.Close();
-            thuTu += 1;
-            bdsFilter += "MAMH = '" + MAMH+ "'";
-            bdsGVDK.Filter = bdsFilter;
-            if (thuTu < 3)
-                bdsFilter += "AND";
+            return MAMH;
+        }
+        public static void kiemTraThuTu(int thuTu,ref String bdsFilter)
+        {
+            
+            if (thuTu > 1 && thuTu <= 3)
+                bdsFilter += " AND ";
+            if (thuTu > 3)
+            {
+                thuTu = 0;
+                bdsFilter = "";
+            }
+        }
+
+        private void cmbTENMONHOC_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbTENMONHOC.SelectedValue != null)
+            {
+                cmbTENMONHOC.Enabled = false;
+                cmbNGAYTHI.Enabled = true;
+                thuTu += 1;
+                if (thuTu == 3)
+                {
+                    lbSOCAUTHI.Text = ((DataRowView)bdsGVDK[0])["SOCAUTHI"].ToString();
+                    lbTRINHDO.Text = ((DataRowView)bdsGVDK[0])["TRINHDO"].ToString();
+                }
+                kiemTraThuTu(thuTu, ref bdsFilter);
+
+                String MAMH = "";
+                MAMH = timMAMH(cmbTENMONHOC.SelectedValue.ToString());
+
+                bdsFilter += "MAMH = '" + MAMH + "'";
+                bdsGVDK.Filter = bdsFilter;
+                cmbLANTHI.SelectedIndex = -1;
+                cmbNGAYTHI.SelectedIndex = -1;
+
+            }
         }
 
         private void cmbNGAYTHI_SelectedIndexChanged(object sender, EventArgs e)
         {
-            thuTu += 1;
-            bdsFilter += "NGAYTHI = '" + cmbNGAYTHI.SelectedValue.ToString() + "'";
-            bdsGVDK.Filter = bdsFilter;
-            if (thuTu < 3)
-                bdsFilter += "AND";
+            if (cmbNGAYTHI.SelectedValue != null)
+            {
+                cmbNGAYTHI.Enabled = false;
+                cmbLANTHI.Enabled = true;
+                thuTu += 1;
+                if (thuTu == 3)
+                {
+                    lbSOCAUTHI.Text = ((DataRowView)bdsGVDK[0])["SOCAUTHI"].ToString();
+                    lbTRINHDO.Text = ((DataRowView)bdsGVDK[0])["TRINHDO"].ToString();
+                }
+                kiemTraThuTu(thuTu, ref bdsFilter);
+                bdsFilter += "NGAYTHI = '" + cmbNGAYTHI.SelectedValue.ToString() + "'";
+                bdsGVDK.Filter = bdsFilter;
+                cmbLANTHI.SelectedIndex = -1;
+            }
         }
 
         private void cmbLANTHI_SelectedIndexChanged(object sender, EventArgs e)
         {
-            thuTu += 1;
-            bdsFilter += "LAN = '" + cmbLANTHI.SelectedValue.ToString() + "'";
-            bdsGVDK.Filter = bdsFilter;
-            if (thuTu < 3)
-                bdsFilter += "AND";
+            if (cmbLANTHI.SelectedValue != null)
+            {
+                cmbLANTHI.Enabled = false;
+                thuTu += 1;
+                if (thuTu == 3)
+                {
+                    lbSOCAUTHI.Text = ((DataRowView)bdsGVDK[0])["SOCAUTHI"].ToString();
+                    lbTRINHDO.Text = ((DataRowView)bdsGVDK[0])["TRINHDO"].ToString();
+                }
+                kiemTraThuTu(thuTu, ref bdsFilter);
+                bdsFilter += "LAN =" + cmbLANTHI.SelectedValue.ToString();
+                bdsGVDK.Filter = bdsFilter;
+                btnBATDAUTHI.Enabled = true;
+            }
+        }
+
+        private void btnCHONLAI_Click(object sender, EventArgs e)
+        {
+            cmbLANTHI.SelectedIndex = -1;
+            cmbNGAYTHI.SelectedIndex = -1;
+            cmbTENMONHOC.SelectedIndex = -1;
+            cmbTENMONHOC.Enabled = true;
+            cmbLANTHI.Enabled = false;
+            cmbNGAYTHI.Enabled = false;
+            lbSOCAUTHI.Text = "";
+            lbTRINHDO.Text = "";
+            thuTu = 0;
+            bdsFilter = "";
         }
     }
 }

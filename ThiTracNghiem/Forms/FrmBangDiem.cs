e0bb07436bfb491de18b75d8b0a70cf7a0b0fef1
Nguyen Tan Dung <TanDung847@gmail.com>
2017-12-05 07:45:47 +0700

    FrmBangdiem

--- ThiTracNghiem/Forms/FrmBangDiem.cs
+++ ThiTracNghiem/Forms/FrmBangDiem.cs
@@ -28,11 +28,51 @@ namespace ThiTracNghiem.Forms
 
         private void FrmBangDiem_Load(object sender, EventArgs e)
         {
+            // TODO: This line of code loads data into the 'dS_SERVER1.V_DS_MONHOC' table. You can move, or remove it, as needed.
+            
+            // TODO: This line of code loads data into the 'dS_SERVER1.V_DS_LOP' table. You can move, or remove it, as needed.
+            
+            // TODO: This line of code loads data into the 'dS_SERVER1.V_DS_MONHOC' table. You can move, or remove it, as needed.
+            //this.v_DS_MONHOCTableAdapter.Fill(this.dS_SERVER1.V_DS_MONHOC);
+            // TODO: This line of code loads data into the 'dS_SERVER1.V_DS_MONHOC' table. You can move, or remove it, as needed.
+            //this.v_DS_MONHOCTableAdapter.Fill(this.dS_SERVER1.V_DS_MONHOC);
+            // TODO: This line of code loads data into the 'dS_SERVER1.V_DS_LOP' table. You can move, or remove it, as needed.
+            //this.v_DS_LOPTableAdapter.Fill(this.dS_SERVER1.V_DS_LOP);
             // TODO: This line of code loads data into the 'dS_SERVER1.BANGDIEM' table. You can move, or remove it, as needed.
             this.dS_SERVER1.EnforceConstraints = false;
             this.bANGDIEMTableAdapter.Connection.ConnectionString = Program.connstr;
             this.bANGDIEMTableAdapter.Fill(this.dS_SERVER1.BANGDIEM);
+            this.v_DS_LOPTableAdapter.Connection.ConnectionString = Program.connstr;
+            this.v_DS_LOPTableAdapter.Fill(this.dS_SERVER1.V_DS_LOP);
+            this.v_DS_MONHOCTableAdapter.Connection.ConnectionString = Program.connstr;
+            this.v_DS_MONHOCTableAdapter.Fill(this.dS_SERVER1.V_DS_MONHOC);
+            List<String> cachXem = new List<String> { "1", "2" };
+            cmbLANTHI.DataSource = cachXem;
+            this.bdsBANGDIEM.Filter = "";
+        }
 
+        private void nGAYTHILabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nGAYTHIDateEdit_EditValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnXem_Click(object sender, EventArgs e)
+        {
+            
+            String Filter = "MAMH = '" + cmbMONHOC.SelectedValue.ToString().Trim() + "',LAN = " + cmbLANTHI.SelectedValue.ToString().Trim();
+            try
+            {
+                //this.bANGDIEMTableAdapter.Fill(this.dS_SERVER1.BANGDIEM);
+                bdsBANGDIEM.Filter = Filter;
+            }catch(Exception exc)
+            {
+                MessageBox.Show("Không có dữ liệu", "", MessageBoxButtons.OK);
+            }
         }
     }
 }
